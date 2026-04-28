8e291847fe9d780eaf8529671030fe58c651cd14
Charlie Boon <187141936+csbdc@users.noreply.github.com>
2026-04-28 22:44:12 +0100

    Adding Cilium

--- terraform/proxmox/modules/talos/main.tf
+++ terraform/proxmox/modules/talos/main.tf
@@ -15,3 +15,11 @@ resource "talos_cluster_kubeconfig" "this" {
   client_configuration = talos_machine_secrets.this.client_configuration
   node                 = var.talos_config.control_planes[var.talos_config.bootstrap_node]
 }
+
+data "talos_client_configuration" "this" {
+  depends_on = [ talos_cluster_kubeconfig.this ]
+  client_configuration = talos_machine_secrets.this.client_configuration
+  cluster_name = var.talos_config.cluster_name
+  endpoints = [ for k,v in var.talos_config.control_planes : v ]
+  nodes = flatten([[ for k,v in var.talos_config.control_planes : v ],[ for k,v in var.talos_config.workers : v ]])
+}
